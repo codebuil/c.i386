60105df7cfb34e3a9c13d68ac71a70a2d9c84979
ana128@sapo.pt <112607693+codebuil@users.noreply.github.com>
2023-05-23 16:05:36 +0100

    Add files via upload

---
+++ rand.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+
+int main(argc,argv)
+int argc;
+char *argv[];
+{
+	int i;
+	int ii;
+	int iii;
+	srand(time(NULL));
+	printf("%d",rand());
+	return 0;
+}
